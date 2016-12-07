119582a95f88cdd9c3f262a1776c81e7dee83597
estherleah <estherleahmorrison@gmail.com>
2016-12-07 23:27:28 +0000

    db updates

--- htdocs/combined/db/dbh.php
+++ htdocs/combined/db/dbh.php
@@ -13,8 +13,8 @@ catch (PDOException $e) {
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "willredmondson@gc0216", "pwd" => "Harvey1Esther2Will3", "Database" => "OCR", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:gc0216.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-echo "connected";
+//$conn = sqlsrv_connect($serverName, $connectionInfo);
+$conn = mysqli_connect('sqlsrv:server = tcp:gc0216.database.windows.net,1433', 'willredmondson', 'Harvey1Esther2Will3', 'OCR');
 
 ?>
 
