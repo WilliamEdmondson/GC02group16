73471ad82b87377e8b5e0c439944211a84d99f57
estherleah <estherleahmorrison@gmail.com>
2016-12-08 13:57:26 +0000

    db

--- htdocs/combined/db/dbh.php
+++ htdocs/combined/db/dbh.php
@@ -1,5 +1,4 @@
 <?php
-
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:gc0216.database.windows.net,1433; Database = OCR", "willredmondson", "Harvey1Esther2Will3");
@@ -9,12 +8,9 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "willredmondson@gc0216", "pwd" => "Harvey1Esther2Will3", "Database" => "OCR", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:gc0216.database.windows.net,1433";
 //$conn = sqlsrv_connect($serverName, $connectionInfo);
 $conn = mysqli_connect('sqlsrv:server = tcp:gc0216.database.windows.net,1433', 'willredmondson', 'Harvey1Esther2Will3', 'OCR');
-
 ?>
-
