2e0745b2878b888baaac5a4b8e88d3899d0ffeb1
HighwayZ <asino1990@live.com>
2016-12-08 13:12:39 +0000

    ui opti & fix sidebar

--- htdocs/combined/sidebar.php
+++ htdocs/combined/sidebar.php
@@ -1,4 +1,3 @@
-
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -10,6 +9,7 @@
     <link href='//fonts.googleapis.com/css?family=Work+Sans:400,500,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
     <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/skycons.js"></script>
 </head>
 <body>
 
@@ -37,62 +37,31 @@
             </li>
 
             </li>
-            <?php
-                if($_SESSION['admin']==1){ ?>
-                    <li><a href="#"><i class="fa fa-user-plus"></i><span>Administrator</span><span class="fa fa-angle-right" style="float: right"></span></a>
-                      <ul id="menu-academico-sub" >
-                        <li> <a href="masquerade.php"><i class="fa fa-user-plus"></i>Profile</a> </li>
-                        <li> <a href="includes/logout.php"><i class="fa fa-sign-out"></i>Logout</a> </li>
-                     </ul>
-                    </li>";
-           <?php }?>
-
+            <li><a href="#"><i class="fa fa-user-plus"></i><span>Administrator</span><span class="fa fa-angle-right" style="float: right"></span></a>
+                <ul id="menu-academico-sub" >
+                    <li> <a href="#"><i class="fa fa-user-plus"></i>Profile</a> </li>
+                    <li> <a href="includes/logout.php"><i class="fa fa-sign-out"></i>Logout</a> </li>
+                </ul>
+            </li>
         </ul>
     </div>
-</div>
-<div class="clearfix"> </div>
-</div>
-<!--scrolling js-->
-<script src="js/jquery.nicescroll.min.js"></script>
-<script src="js/script.js"></script>
-<!--//scrolling js-->
 
-<script type="text/javascript"  src="js/bootstrap.js"> </script>
+</div>
 
-<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
 <script>
     $(document).ready(function() {
-        var id = '#dialog';
-//Get the screen height and width
-        var maskHeight = $(document).height();
-        var maskWidth = $(window).width();
-        var mask = $('#mask');
-//Set heigth and width to mask to fill up the whole screen
-        mask.css({'width':maskWidth,'height':maskHeight});
-//transition effect
-        mask.fadeIn(500);
-        mask.fadeTo("slow",0.9);
-//Get the window height and width
-        var winH = $(window).height();
-        var winW = $(window).width();
-//Set the popup window to center
-        $(id).css('top',  winH/2-$(id).height()/2);
-        $(id).css('left', winW/2-$(id).width()/2);
-//transition effect
-        $(id).fadeIn(2000);
-//if close button is clicked
-        $('.window .close').click(function (e) {
-//Cancel the link behavior
-            e.preventDefault();
-            $('#mask').hide();
-            $('.window').hide();
+        var navoffeset=$(".sidebar").offset().top;
+        $(window).scroll(function(){
+            var scrollpos=$(window).scrollTop();
+            if(scrollpos >=navoffeset){
+                $(".sidebar").addClass("fixed");
+            }else{
+                $(".sidebar").removeClass("fixed");
+            }
         });
-//if mask is clicked
-        mask.click(function () {
-            $(this).hide();
-            $('.window').hide();
-        });
+
     });
 </script>
+
 
 </body>
