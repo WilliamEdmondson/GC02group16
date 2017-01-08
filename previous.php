192d60b92d0272a26440670db42ea157b6b7f1a0
HighwayZ <asino1990@live.com>
2017-01-08 22:07:54 +0000

    add print all/print table only
    add print all/print table only

---
+++ previous.php
@@ -0,0 +1,163 @@
+<?php
+session_start();
+?>
+<html>
+<div id =sidebar class="visible">
+    <?php include("sidebar.php"); ?>
+</div>
+
+
+<head>
+<title>Sample previous work</title>
+	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+    <link href="css/bootstrap-combined.min.css" rel="stylesheet" media="screen">
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="page-container">
+    <div class="left-content">
+        <div class="mother-grid-inner">
+            <!--header start here-->
+            <?php include("header.php"); ?>
+            <br>
+            <!--header start here-->
+
+            <div class="container-fluid">
+                <div class="row-fluid">
+                    <div class="span12">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a data-toggle ="tab" href="#chart">Chart</a></li>
+                            <li><a data-toggle ="tab" href="#table">Table</a></li>
+                        </ul>
+                    </div>
+			    <div id="myTabContent" class="tab-content">
+
+                    <div id="chart" class="tab-pane fade in active">
+
+                        <div class="row-fluid">
+                            <?php
+                            include "data.php";
+                            ?>
+                            <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+                            <script type="text/javascript" src="js/Chart.min.js"></script>
+                            <?php
+	                        $json_array = $_SESSION['json_array'];
+                            //	echo implode("<br>",$json_array);
+	                        for ($i=0; $i < 20; $i++) {
+	                        ?><div class="col-xs-3 span3"><?php
+		                    $data = $json_array[$i];//		echo $current_json;
+	                        $question = $i+1;
+	                        echo "Question $question:<br><br>";
+                            //	echo $data;
+	                        echo "<canvas id='mycanvas".$i."' style='width:360; height:360'></canvas>";
+	                        echo "<script language='javascript'>
+	                        var data = $data;
+                            var label = [];
+                            var total = [];
+                            for(var j in data) {
+                            label.push(data[j].label);
+                            total.push(data[j].total);
+                            }
+                            var ctx = document.getElementById('mycanvas".$i."');
+                            var mycanvas".$i." = new Chart(ctx, {
+                            type: 'pie',
+                            data: {
+                            labels: label,
+                            datasets: [{
+                            label: 'Question ' + $question,
+                            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
+                            borderColor: ['rgba(255,99,132,1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(54, 162, 235, 1)', 'rgba(153, 102, 255, 1)'],
+                            data: total
+                            }
+                            ]
+                            },
+                            options: {
+                                responsive: false,
+                                    scales: {
+                                        yAxes: [{
+                                             ticks: {
+                                                   beginAtZero:true
+                                                    }
+                                                }]
+                                            }
+                                        }
+                                    
+                                    });
+
+	                        </script>";
+                             ?><br><br><br></div><?php
+                            }
+                            ?>
+                    </div>
+
+                </div>
+                <div id="table" class="tab-pane fade in active">
+                    <input id="btnPrint" style="float:right" type="button" value="Print all" onclick="javascript:window.print();" />
+                    <input id="btnPrint"  style="float:right; margin-right:10px; " type="button" value="Print table" onclick=preview(1) />
+                    <!--                        <style type="text/css" media=print>-->
+                    <!--                            .noprint{display : none }-->
+                    <!--                        </style>-->
+                    <script>
+                        function preview(oper)
+                        {
+                            if (oper < 10)
+                            {
+                                bdhtml=window.document.body.innerHTML;
+                                sprnstr="<!--startprint"+oper+"-->";
+                                eprnstr="<!--endprint"+oper+"-->";
+                                prnhtml=bdhtml.substring(bdhtml.indexOf(sprnstr)+18);
+
+                                prnhtml=prnhtml.substring(0,prnhtml.indexOf(eprnstr));
+                                window.document.body.innerHTML=prnhtml;
+                                window.print();
+                                window.document.body.innerHTML=bdhtml;
+                            } else {
+                                window.print();
+                            }
+                        }
+                    </script>
+
+                    <!--startprint1-->
+<?php
+mysql_connect('localhost', 'root', '');
+mysql_select_db('quexf');
+
+$bgid;
+
+for ($i=0; $i < 20; $i++) {
+	$bgid = $i + 1;
+	$sql="SELECT label, COUNT(f.bid) AS total
+	FROM boxes b LEFT JOIN formboxverifychar f ON b.bid = f.bid
+	WHERE b.bgid=$bgid
+	GROUP BY label
+	ORDER BY b.bid";
+	$records = mysql_query($sql);
+
+	echo "Question $bgid";
+
+	echo "<table style = 'margin:20; border:1px solid black'>";
+
+	echo "<tr>";
+		echo "<th style = 'padding:5px; border:1px solid grey'>Option</th>";
+		echo "<th style = 'padding:5px; border:1px solid grey'>Total</th>";
+	echo "</tr>";
+
+	while ($quexf=mysql_fetch_assoc($records)) {
+		echo "<tr>";
+		echo "<td style = 'padding:5px;'>".$quexf['label']."</td>";
+		echo "<td style = 'padding:5px;'>".$quexf['total']."</td>";
+		echo "</tr>";
+	}
+	echo "</table>";
+}
+?>
+                </div>
+            </div>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div><!--endprint1-->
+</body>
+</html>
